a654a725761c0bbbd4bd124aa3b78e441ac8ff10
renyi <541421397@qq.com>
2019-10-09 20:48:17 +0800

    在家push昨晚的

---
+++ basis/1/forLoop.java
@@ -0,0 +1,10 @@
+class forLoop {
+    public static void main(String[] args) {
+        char ch = '1';
+        char b = (char) 200;
+        while (ch < b) {
+            ch++;
+            System.out.println(ch);
+        }
+    }
+}
---
+++ basis/1/zizeng.java
@@ -0,0 +1,5 @@
+public class zizeng {
+    public static void main(String[] args) {
+        System.out.println(false || false);
+    }
+}
